4ec59e9da36dac0554b3be4f178eb89d657a45be
TasG99 <9861@holbertonstudents.com>
2025-05-11 11:42:45 +0000

    Debugging - Python Factorial

--- factorial.py
+++ factorial.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
+
 import sys
 
 def factorial(n):
     result = 1
     while n > 1:
         result *= n
-        n -= 1
+        n -= 1  # Decrement n to avoid infinite loop
     return result
 
-if __name__ == "__main__":
-    num = int(sys.argv[1])
-    print(factorial(num)) 
+f = factorial(int(sys.argv[1]))
+print(f) 
