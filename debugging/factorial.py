ab90104477eb81346dac8c0b3684c9ef19094d22
TasG99 <9861@holbertonstudents.com>
2025-05-18 06:10:47 +0000

    Debugging - Python Factorial

--- debugging/factorial.py
+++ debugging/factorial.py
@@ -1,13 +1,22 @@
 #!/usr/bin/python3
-
 import sys
 
 def factorial(n):
     result = 1
     while n > 1:
         result *= n
-        n = n - 1
+        n -= 1
     return result
 
-f = factorial(int(sys.argv[1]))
-print(f)
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: ./script.py <non-negative integer>")
+        sys.exit(1)
+    try:
+        num = int(sys.argv[1])
+        if num < 0:
+            raise ValueError
+        print(factorial(num))
+    except ValueError:
+        print("Please provide a valid non-negative integer.")
+        sys.exit(1)
